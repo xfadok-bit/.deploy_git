92892d01c6bb7d879c1c2b2a229733679578a6c1
xfadok-bit <xfadok@gmail.com>
2025-12-29 16:07:18 +0800

    Site updated: 2025-12-29 16:07:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/29/hello-world/","2025/12/29/美女与野兽的战斗/"];function toRandomPost(){
+var posts=["2025/12/29/美女与野兽的战斗/","2025/12/29/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
