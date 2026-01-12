5853e3a7cad4ce0a65d853d0dd7c614f1f83e02c
xfadok-bit <xfadok@gmail.com>
2026-01-13 01:47:41 +0800

    Site updated: 2026-01-13 01:47:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/29/美女与野兽的战斗/","2025/12/29/hello-world/"];function toRandomPost(){
+var posts=["2026/01/11/喜欢穿可爱内裤的可爱屁股/","2026/01/11/如何重新开始/","2025/12/29/美女与野兽的战斗/","2025/12/29/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
